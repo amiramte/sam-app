6409f94eb035e730fc20c9605164ab4d7d6a0dc5
Amir <71940532+amiramte@users.noreply.github.com>
2021-05-18 02:35:41 +0430

    hanchin

---
+++ pipeline/lib/pipeline-stack.ts
@@ -0,0 +1,15 @@
+import * as cdk from '@aws-cdk/core';
+import s3 = require('@aws-cdk/aws-s3');
+import codecommit = require('@aws-cdk/aws-codecommit');
+import codepipeline = require('@aws-cdk/aws-codepipeline');
+import codepipeline_actions = require('@aws-cdk/aws-codepipeline-actions');
+import codebuild = require('@aws-cdk/aws-codebuild');
+
+export class PipelineStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const artifactsBucket = new s3.Bucket(this, "ArtifactsBucket");
+  }
+}
